26403c3a568ba882600e99d529a63b73d3c28ac8
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-13 11:13:38 +0300

    Не добавляет пагинацию для mobile

--- build/js/main-slider.js
+++ build/js/main-slider.js
@@ -12,30 +12,55 @@
       spaceBetween: 30,
       centeredSlidesBounds: true,
 
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-      },
-
       navigation: {
         prevEl: '.novelty__buttons-item--prev',
         nextEl: '.novelty__buttons-item--next',
+      },
+
+      pagination: {
+        el: '.swiper-pagination',
+        renderFraction: function (currentClass, totalClass) {
+          return `<span class="${currentClass}"></span> of <span class="${totalClass}"></span>`;
+        }
       },
 
       breakpoints: {
         320: {
           slidesPerView: 2,
           slidesPerGroup: 2,
+
+          pagination: {
+            el: '.swiper-pagination',
+            renderFraction: function (currentClass, totalClass) {
+              return `<span class="${currentClass}"></span> of <span class="${totalClass}"></span>`;
+            }
+          },
         },
+        768: {
+/*          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },*/
+        },
+
         1024: {
           slidesPerView: 4,
           slidesPerGroup: 4,
+/*
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },*/
         },
       },
     });
   }
+
   initSwiper();
 })();
--- source/js/main-slider.js
+++ source/js/main-slider.js
@@ -12,30 +12,41 @@
       spaceBetween: 30,
       centeredSlidesBounds: true,
 
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-      },
-
       navigation: {
         prevEl: '.novelty__buttons-item--prev',
         nextEl: '.novelty__buttons-item--next',
       },
+
 
       breakpoints: {
         320: {
           slidesPerView: 2,
           slidesPerGroup: 2,
         },
+        768: {
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },
+        },
+
         1024: {
           slidesPerView: 4,
           slidesPerGroup: 4,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },
         },
       },
     });
   }
+
   initSwiper();
 })();
