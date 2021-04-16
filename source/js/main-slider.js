af8f2ec2f433cdc4eed85c5c69fc3b8d314e4ccb
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-16 21:34:17 +0300

    Добавляет активные состояния в slider

--- source/js/main-slider.js
+++ source/js/main-slider.js
@@ -17,16 +17,26 @@
         nextEl: '.novelty__buttons-item--next',
       },
 
-
       breakpoints: {
         320: {
           slidesPerView: 2,
           slidesPerGroup: 2,
+
+          pagination:{
+            type: 'fraction',
+            el: '.swiper-pagination',
+            renderFraction: function (currentClass, totalClass) {
+              return '<span class="' + currentClass + '"></span>' +
+                '<span class="swiper-pagination-of">\t&nbsp;of\t&nbsp;</span>'+
+                '<span class="' + totalClass + '"></span>';
+            }
+          },
         },
         768: {
           pagination: {
+            type: 'bullets',
             el: '.swiper-pagination',
-            clickable: true,
+            clickable: 'true',
             renderBullet: function (index, className) {
               return '<span class="' + className + '">' + (index + 1) + '</span>';
             },
@@ -36,9 +46,11 @@
         1024: {
           slidesPerView: 4,
           slidesPerGroup: 4,
+
           pagination: {
+            type: 'bullets',
             el: '.swiper-pagination',
-            clickable: true,
+            clickable: 'true',
             renderBullet: function (index, className) {
               return '<span class="' + className + '">' + (index + 1) + '</span>';
             },
