c821c210a35739c0b4b1b8aca7841c7f1f234028
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-18 11:59:11 +0300

    Добавляет nojs в filter

--- build/js/filter.js
+++ build/js/filter.js
@@ -2,44 +2,53 @@
 
 (function () {
   const filterForm = document.querySelector('.filter-form');
-  const BTN_CLASS_NAME = '.filter-form__open-link';
-  const BTN_CLASS_NAME_OPENED = 'filter-form__open-link--opened';
-  const CLASS_NAME_HIDDEN = 'visually-hidden';
-  const linkOpenFilter = document.querySelector('.catalog__link-to-filter');
-  const closeBtn = filterForm.querySelector('.filter-form__close');
+  const toggleDetails = '.filter-form__open-link';
+  const toggleOpened = 'filter-form__open-link--opened';
+  const closedDetails = 'filter-form__check-container--closed';
+
+  document.querySelectorAll(toggleDetails).forEach(function (item) {
+   if(!item.classList.contains(toggleOpened)){
+     item.nextElementSibling.classList.add(closedDetails);
+   }
+  });
+
+  /*
+    const linkOpenFilter = document.querySelector('.catalog__link-to-filter');
+    const closeBtn = filterForm.querySelector('.filter-form__close');
+  */
 
   function openDetails(evt) {
-    if (!evt.target.matches(BTN_CLASS_NAME)) {
+    if (!evt.target.matches(toggleDetails)) {
       return;
     }
     let details = evt.target.nextElementSibling;
-    if (evt.target.classList.contains(BTN_CLASS_NAME_OPENED)) {
-      details.classList.add(CLASS_NAME_HIDDEN);
-      evt.target.classList.remove(BTN_CLASS_NAME_OPENED);
+    if (evt.target.classList.contains(toggleOpened)) {
+      details.classList.add(closedDetails);
+      evt.target.classList.remove(toggleOpened);
     } else {
-      details.classList.remove(CLASS_NAME_HIDDEN);
-      evt.target.classList.add(BTN_CLASS_NAME_OPENED);
+      details.classList.remove(closedDetails);
+      evt.target.classList.add(toggleOpened);
     }
   }
 
-  function onCloseFilter(evt) {
-    if (evt.key !== 'Escape' && !evt.target.classList.contains('filter-form__close')) {
-      return;
-    }
-    filterForm.classList.remove('filter-form--opened');
-  }
-
-  function openFilter(evt) {
-    evt.preventDefault();
-    if (filterForm.classList.contains('filter-form--opened')) {
+  /*  function onCloseFilter(evt) {
+      if (evt.key !== 'Escape' && !evt.target.classList.contains('filter-form__close')) {
+        return;
+      }
       filterForm.classList.remove('filter-form--opened');
-    } else {
-      filterForm.classList.add('filter-form--opened');
-      closeBtn.addEventListener('click', onCloseFilter);
-      document.addEventListener('keydown', onCloseFilter);
-    }
-  }
+    }*/
+
+  /*  function openFilter(evt) {
+      evt.preventDefault();
+      if (filterForm.classList.contains('filter-form--opened')) {
+        filterForm.classList.remove('filter-form--opened');
+      } else {
+        filterForm.classList.add('filter-form--opened');
+        closeBtn.addEventListener('click', onCloseFilter);
+        document.addEventListener('keydown', onCloseFilter);
+      }
+    }*/
 
   filterForm.addEventListener('click', openDetails);
-  linkOpenFilter.addEventListener('click', openFilter);
+  // linkOpenFilter.addEventListener('click', openFilter);
 })();
