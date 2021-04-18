3721bc5d57d45cc9c40c286ef529555ba90d889e
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-18 16:41:30 +0300

    Добавляет валидацию range

--- build/js/filter.js
+++ build/js/filter.js
@@ -9,12 +9,14 @@
   const filterForm = document.querySelector('.filter-form');
   const linkOpenFilter = document.querySelector('.catalog__link-to-filter');
   const closeBtn = filterForm.querySelector('.filter-form__close');
+  const inputLowPrice = document.querySelector('#low-price');
+  const inputHighPrice = document.querySelector('#high-price');
 
   document.querySelectorAll('.filter-form__box').forEach(function (item) {
     if (item.dataset.toggle === '1') {
       item.firstElementChild.classList.add(TOGGLE_OPENED_CLASS);
     } else {
-     item.lastElementChild.classList.add(CLOSED_DETAILS_CLASS);
+      item.lastElementChild.classList.add(CLOSED_DETAILS_CLASS);
     }
   });
 
@@ -32,24 +34,33 @@
     }
   }
 
-    function onCloseFilter(evt) {
-      if (evt.key !== 'Escape' && !evt.target.classList.contains(FILTER_CLOSED_CLASS)) {
-        return;
-      }
-      filterForm.classList.remove(FILTER_OPENED_CLASS);
+  function onCloseFilter(evt) {
+    if (evt.key !== 'Escape' && !evt.target.classList.contains(FILTER_CLOSED_CLASS)) {
+      return;
     }
+    filterForm.classList.remove(FILTER_OPENED_CLASS);
+  }
 
-    function openFilter(evt) {
-      evt.preventDefault();
-      if (filterForm.classList.contains(FILTER_OPENED_CLASS)) {
-        filterForm.classList.remove(FILTER_OPENED_CLASS);
-      } else {
-        filterForm.classList.add(FILTER_OPENED_CLASS);
-        closeBtn.addEventListener('click', onCloseFilter);
-        document.addEventListener('keydown', onCloseFilter);
-      }
+  function openFilter(evt) {
+    evt.preventDefault();
+    if (filterForm.classList.contains(FILTER_OPENED_CLASS)) {
+      filterForm.classList.remove(FILTER_OPENED_CLASS);
+    } else {
+      filterForm.classList.add(FILTER_OPENED_CLASS);
+      closeBtn.addEventListener('click', onCloseFilter);
+      document.addEventListener('keydown', onCloseFilter);
     }
+  }
+
+  function onRangeInput(evt) {
+    var max = evt.currentTarget.value;
+    if (evt.currentTarget.value.length > 3) {
+      evt.currentTarget.value =  max.slice(0,3);
+    }
+  }
 
   filterForm.addEventListener('click', openDetails);
   linkOpenFilter.addEventListener('click', openFilter);
+  inputLowPrice.addEventListener('input', onRangeInput);
+  inputHighPrice.addEventListener('input', onRangeInput);
 })();
