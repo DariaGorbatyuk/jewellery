137f1e3b50d3747f03a459b312c86880b60e2df6
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-18 12:17:11 +0300

    filter.js рефакторинг

--- source/js/filter.js
+++ source/js/filter.js
@@ -2,55 +2,55 @@
 
 (function () {
   const filterForm = document.querySelector('.filter-form');
-  const toggleDetails = '.filter-form__open-link';
-  const toggleOpened = 'filter-form__open-link--opened';
-  const closedDetails = 'filter-form__check-container--closed';
+
+  const TOGGLE_DETAILS_CLASS = '.filter-form__open-link';
+  const TOGGLE_OPENED_CLASS = 'filter-form__open-link--opened';
+  const CLOSED_DETAILS_CLASS = 'filter-form__check-container--closed';
+  const FILTER_CLOSED_CLASS = 'filter-form__close';
+  const FILTER_OPENED_CLASS = 'filter-form--opened';
+  const linkOpenFilter = document.querySelector('.catalog__link-to-filter');
+  const closeBtn = filterForm.querySelector('.filter-form__close');
 
   document.querySelectorAll('.filter-form__box').forEach(function (item) {
     if (item.dataset.toggle === '1') {
-      item.firstElementChild.classList.add(toggleOpened);
+      item.firstElementChild.classList.add(TOGGLE_OPENED_CLASS);
     } else {
-     item.lastElementChild.classList.add(closedDetails);
+     item.lastElementChild.classList.add(CLOSED_DETAILS_CLASS);
     }
   });
 
-  /*
-    const linkOpenFilter = document.querySelector('.catalog__link-to-filter');
-    const closeBtn = filterForm.querySelector('.filter-form__close');
-  */
-
   function openDetails(evt) {
-    if (!evt.target.matches(toggleDetails)) {
+    if (!evt.target.matches(TOGGLE_DETAILS_CLASS)) {
       return;
     }
     let details = evt.target.nextElementSibling;
-    if (evt.target.classList.contains(toggleOpened)) {
-      details.classList.add(closedDetails);
-      evt.target.classList.remove(toggleOpened);
+    if (evt.target.classList.contains(TOGGLE_OPENED_CLASS)) {
+      details.classList.add(CLOSED_DETAILS_CLASS);
+      evt.target.classList.remove(TOGGLE_OPENED_CLASS);
     } else {
-      details.classList.remove(closedDetails);
-      evt.target.classList.add(toggleOpened);
+      details.classList.remove(CLOSED_DETAILS_CLASS);
+      evt.target.classList.add(TOGGLE_OPENED_CLASS);
     }
   }
 
-  /*  function onCloseFilter(evt) {
-      if (evt.key !== 'Escape' && !evt.target.classList.contains('filter-form__close')) {
+    function onCloseFilter(evt) {
+      if (evt.key !== 'Escape' && !evt.target.classList.contains(FILTER_CLOSED_CLASS)) {
         return;
       }
-      filterForm.classList.remove('filter-form--opened');
-    }*/
+      filterForm.classList.remove(FILTER_OPENED_CLASS);
+    }
 
-  /*  function openFilter(evt) {
+    function openFilter(evt) {
       evt.preventDefault();
-      if (filterForm.classList.contains('filter-form--opened')) {
-        filterForm.classList.remove('filter-form--opened');
+      if (filterForm.classList.contains(FILTER_OPENED_CLASS)) {
+        filterForm.classList.remove(FILTER_OPENED_CLASS);
       } else {
-        filterForm.classList.add('filter-form--opened');
+        filterForm.classList.add(FILTER_OPENED_CLASS);
         closeBtn.addEventListener('click', onCloseFilter);
         document.addEventListener('keydown', onCloseFilter);
       }
-    }*/
+    }
 
   filterForm.addEventListener('click', openDetails);
-  // linkOpenFilter.addEventListener('click', openFilter);
+  linkOpenFilter.addEventListener('click', openFilter);
 })();
