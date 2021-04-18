2a529ffb56034548e9a912ba695191e43d599e75
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-18 14:00:13 +0300

    Рефакторинг faq js

--- build/js/faq.js
+++ build/js/faq.js
@@ -3,10 +3,13 @@
 (function () {
   const CONTENT_CLOSED_CLASS = 'faq-el__content--closed';
   const TITLE_CLOSED_CLASS = 'faq-el__title--closed';
-  const faqList = document.querySelector('.faq__list');
+  const FAQ_LIST_CLASS = 'faq__list';
+  const FAQ_EL_CLASS = 'faq-el';
+  const FAQ_LINK_CLASS = 'faq-el__link'
+  const faqList = document.querySelector('.' + FAQ_LIST_CLASS);
 
   function startFaq() {
-    document.querySelectorAll('.faq-el').forEach(function (item) {
+    document.querySelectorAll('.' + FAQ_EL_CLASS).forEach(function (item) {
       if (item.dataset.toggle === '0') {
         item.querySelector('.faq-el__title').classList.add(TITLE_CLOSED_CLASS);
         item.querySelector('.faq-el__content').classList.add(CONTENT_CLOSED_CLASS);
@@ -15,7 +18,7 @@
   }
 
   function onToggleFAQ(evt) {
-    if (!evt.target.classList.contains('faq-el__link') && evt.key !== 'Enter') {
+    if (!evt.target.classList.contains(FAQ_LINK_CLASS) && evt.key !== 'Enter') {
       return;
     }
     if (evt.target.querySelector('.faq-el__title').classList.contains(TITLE_CLOSED_CLASS)) {
