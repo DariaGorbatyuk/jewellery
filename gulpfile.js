dd7f00f3e45366f6fc7e0de5b9d4df8e6df09306
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-04-22 13:50:42 +0300

    Минимизирует картинки

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('images', function () {
   return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
+      imagemin.jpegtran({quality: 75, progressive: true}),
       imagemin.svgo(),
     ]))
 
@@ -64,7 +64,6 @@ gulp.task('images', function () {
 gulp.task('webp', function () {
   return gulp.src('build/img/**/*.{png,jpg}')
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest('build/img'))
     .pipe(gulp.dest('source/img'));
 });
 
@@ -99,5 +98,5 @@ gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy','webp', 'css', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'images', 'webp','copy', 'css', 'sprite', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
